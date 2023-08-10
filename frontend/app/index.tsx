abd9d088649e98fad0a2762fb7471a00a66ee292
LinhTran263 <90758691+LinhTran263@users.noreply.github.com>
2023-08-10 16:53:11 +0700

    fetch data

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,8 +1,60 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import { YStack } from "tamagui";
 import { LogoScreen } from "../src/components/Logo";
+import { useEffect, useState } from "react";
 
 export default () => {
+  const router = useRouter();
+
+  const [avail, setAvail] = useState([])
+
+  const fetchAvailData = () => {
+    fetch("https://64c881f3a1fe0128fbd5db6f.mockapi.io/available")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setAvail(data)
+      })
+  }
+  
+  useEffect(() => {
+    fetchAvailData()
+  }, [])
+
+  fetch('https://64c881f3a1fe0128fbd5db6f.mockapi.io/posts/1', {
+    method: 'PUT', // or PATCH
+    headers: {'content-type':'application/json'},
+    body: JSON.stringify({available: avail})
+  }).then(res => {
+    if (res.ok) {
+        return res.json();
+    }
+    // handle error
+  }).then(task => {
+    fetch('https://64c881f3a1fe0128fbd5db6f.mockapi.io/posts/1', {
+      method: 'PUT', // or PATCH
+      headers: {'content-type':'application/json'},
+      body: JSON.stringify({basket: task.basketList.length})
+    }).then(res => {
+      if (res.ok) {
+          return res.json();
+      }
+      // handle error
+    }).then(task => {
+      // console.log(task);
+      // Do something with updated task
+    }).catch(error => {
+      // handle error
+    })
+
+    // console.log(task);
+    // console.log(task);
+  }).catch(error => {
+    // handle error
+  })
+
+
   return (
     <YStack
       flex={1}
